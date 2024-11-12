ac1df21cb54d770cd41ebdd3daebbcf1a8a5de3e
valtronic <valentin.yeo@gridapp.co>
2024-11-12 19:52:47 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FocusWheel from './FocusWheel';
+
+function App() {
+  return (
+    <div className="App">
+      <FocusWheel />
+    </div>
+  );
+}
+
+export default App;
